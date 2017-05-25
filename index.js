c9214735c45175428ed8e2e4c537015a22ad8ff3
Wolfgang Beer <wolfgang@app-dev.io>
2017-05-25 23:47:34 +0200

    fixed dependency injection in index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const config = require("./config.js");
 
 const mongodb = new TMSF.MongoDB(config.dependencies.mongodb);
 const authProvider = new TMSF.AuthProvider(config.tokenSecret);
-const service = new TMSF.Service(config, mongodb, authProvider);
+const dependencies = { mongodb, authProvider };
+const service = new TMSF.Service(config, dependencies);
 const server = service.start();

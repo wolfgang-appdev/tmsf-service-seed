e8a25aceb688de5964403e7ad5e8d2558165dc89
Wolfgang Beer <wolfgang@app-dev.io>
2017-05-25 22:28:55 +0200

    changed handler naming convention

--- handler/router.js
+++ handler/router.js
@@ -1,11 +1,11 @@
-const index = require("./index");
+const get_index = require("./get_index");
 
 module.exports = {
     getRoutes: (express, dependencies) => {
         const router = express.Router();
         const { authProvider } = dependencies;
 
-        router.get("/", ...index.middlewares, (req, res) => index.handler(req, res, dependencies));
+        router.get("/", ...get_index.middlewares, (req, res) => get_index.handler(req, res, dependencies));
         router.use(authProvider.requireToken);
         router.get("/protected", (req, res) => console.log("Hello from protected route"));
 

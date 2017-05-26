cc5de0ce079a0330def9871758cd120bb90426b3
Wolfgang Beer <wolfgang@app-dev.io>
2017-05-26 13:43:04 +0200

    created settings field in config.js

--- config.js
+++ config.js
@@ -3,13 +3,15 @@ const models = require("./models");
 
 module.exports = {
 
-    nodeEnv: process.env.NODE_ENV || "development",
-    tokenSecret: process.env.TOKEN_SECRET || "super_duper_secret",
-    router: router,
+    settings: {
+        nodeEnv: process.env.NODE_ENV || "development",
+        tokenSecret: process.env.TOKEN_SECRET || "super_duper_secret",
+        router: router,
 
-    endpoint: {
-        host: process.env.NODE_HOST || "0.0.0.0",
-        port: process.env.NODE_PORT || 3000
+        endpoint: {
+            host: process.env.NODE_HOST || "0.0.0.0",
+            port: process.env.NODE_PORT || 3000
+        }
     },
 
     dependencies: {
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TMSF = require("tmsf");
 const config = require("./config.js");
 
 const mongodb = new TMSF.MongoDB(config.dependencies.mongodb);
-const authProvider = new TMSF.AuthProvider(config.tokenSecret);
+const authProvider = new TMSF.AuthProvider(config.settings.tokenSecret);
 const dependencies = { mongodb, authProvider };
-const service = new TMSF.Service(config, dependencies);
+const service = new TMSF.Service(config.settings, dependencies);
 const server = service.start();
